cc9b780d5baede341f5bd374255b0ed5707ad7a5
quyetvv@gmail.com <quyet231>
2016-11-28 16:08:18 +0700

    Redis client (StackExchange.Redis) pub/sub example
    Two process communicates via pub/sub same channel of redis

---
+++ RedisPublisher/Program.cs
@@ -0,0 +1,47 @@
+﻿using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RedisPublisher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var SingleHost = "";
+            int DefaultPublishMessageCount = 10, numberMessages;
+            var ChannelName = "ChannelName";
+            // using ()
+            ConnectionMultiplexer redis = ConnectionMultiplexer.Connect("localhost");
+            ISubscriber sub = redis.GetSubscriber();
+            while (true)
+            {                                
+                Console.WriteLine("Input numbner of publishing messages...");
+                Console.WriteLine("Enter numbner of publishing messages or q to quit");
+                Int32.TryParse(Console.ReadLine(), out numberMessages);
+                if (numberMessages > 0)
+                {
+                    for (var i = 1; i <= numberMessages; i++)
+                    {
+                        Task.Factory.StartNew(() =>
+                        {
+                            Thread.Sleep(30);
+                            var now = DateTime.Now;
+                            string message = ChannelName + now.ToLongTimeString() + "   " + now.Millisecond;
+                            Console.WriteLine("Publishing '{0}' to '{1}'", message, ChannelName);
+                            sub.Publish(ChannelName, message);
+                        });
+                    };
+                }
+                if (Console.ReadKey().KeyChar == 'q')
+                {
+                    break;
+                }
+            };
+        }
+    }
+}
---
+++ RedisSubscriber/Program.cs
@@ -0,0 +1,36 @@
+﻿using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RedisSubscriber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                var SingleHost = "";
+                var PublishMessageCount = 3;
+                var ChannelName = "ChannelName";
+                // using ()
+                ConnectionMultiplexer redis = ConnectionMultiplexer.Connect("localhost");
+                ISubscriber sub = redis.GetSubscriber();
+
+                sub.Subscribe(ChannelName, (channel, message) =>
+                {
+                    Console.WriteLine((string)message);
+                });
+
+                if (Console.ReadKey().KeyChar == 'q')
+                {
+
+                }
+            }
+        }
+    }
+}
